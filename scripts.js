f421147b6cf9e57a8241fdadbab316b1d8c7d7e1
Andrew <70990567+apakula036@users.noreply.github.com>
2022-04-03 15:49:15 -0700

    Added functionality
    Added IDs for specific items and has core functionality ready. Main function for finding price and any other necessary data is there.

--- scripts.js
+++ scripts.js
@@ -1,44 +1,46 @@
 const axios = require('axios');
 
 
-const ranarrSeed = "5295";//ranarr
+const ranarrSeed = "5295";//ranarr seed 
+const ranarrHerb = "257"; //ranarr herb
 
 
+ranarrHerbFunction();
+ranarrSeedFunction();
 
-//seedPrices();
-highAlchPrices(ranarrSeed);
 
-
-//hard coded ids for Ranarr seed and herbs
-function seedPrices(){
-    axios.get("https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="+ 5295)
+function findPriceOfItem(itemID){
+    axios.get("https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="+ itemID)
     .then((res) => { 
-           console.log("Grabbing price...");
-           ranarrSeedPrice = res.data.item.current.price;
+        var priceOfItem = res.data.item.current.price;//note stored as string need to parse as int before doing math 
+        console.log(priceOfItem);
+        return res.data.item.current.price;
     })
     .catch((err) => {
         console.error('ERR:', err)
     })
+}
+function ranarrHerbFunction(){
     axios.get("https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="+ 257)
     .then((res) => { 
-           console.log("Grabbing price...");
-           ranarrHerbPrice = res.data.item.current.price;
+        var priceOfItem = res.data.item.current.price;
+        var newInterger = parseInt(priceOfItem);
+        console.log((newInterger * 7) + "k Is the sell price of 7 herbs.");
+        return newInterger;
     })
     .catch((err) => {
         console.error('ERR:', err)
     })
-    console.log(ranarrSeedPrice - (ranarrHerbPrice * 7) + "= seed price - herb price * 7");
-}
-
-
-function highAlchPrices(item){
-    axios.get("https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="+ item)
+}function ranarrSeedFunction(){
+    axios.get("https://services.runescape.com/m=itemdb_oldschool/api/catalogue/detail.json?item="+ 5295)
     .then((res) => { 
-        console.log(res.data.item.current.price);
+        var priceOfItem = res.data.item.current.price;
+        var newInteger = parseInt(priceOfItem);
 
+        console.log(priceOfItem + " is the cost of the seed.");
+        return newInteger;
     })
     .catch((err) => {
-        console.error('ERR:', err)
-        
+        console.error('ERR:', err)  
     })
 }
